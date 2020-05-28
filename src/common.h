92367619b7ba7e714c579a3dc2605ab2d0bd1a41
Ritchie <frank_ruizhi@163.com>
2020-05-28 17:46:07 +0800

    Update common.h
    sync updates from https://github.com/happyfish100/fastdfs-nginx-module

--- src/common.h
+++ src/common.h
@@ -3,14 +3,14 @@
 *
 * FastDFS may be copied only under the terms of the GNU General
 * Public License V3, which may be found in the FastDFS source kit.
-* Please visit the FastDFS Home Page http://www.csource.org/ for more detail.
+* Please visit the FastDFS Home Page http://www.fastken.com/ for more detail.
 **/
 
 #ifndef FDFS_MOD_COMMON_H
 #define FDFS_MOD_COMMON_H
 
 #include <time.h>
-#include "tracker_types.h"
+#include "fastdfs/tracker_types.h"
 
 #ifndef HTTP_OK
 #define HTTP_OK                    200
@@ -44,6 +44,10 @@
 #define HTTP_NOTFOUND              404
 #endif
 
+#ifndef HTTP_RANGE_NOT_SATISFIABLE
+#define HTTP_RANGE_NOT_SATISFIABLE 416
+#endif
+
 #ifndef HTTP_INTERNAL_SERVER_ERROR
 #define HTTP_INTERNAL_SERVER_ERROR 500
 #endif
@@ -55,6 +59,9 @@
 #ifndef FDFS_STORAGE_STORE_PATH_PREFIX_CHAR
 #define FDFS_STORAGE_STORE_PATH_PREFIX_CHAR  'M'
 #endif
+
+#define FDFS_MAX_HTTP_RANGES    5
+#define FDFS_MAX_HTTP_BOUNDARY 20
 
 #ifdef __cplusplus
 extern "C" {
@@ -71,20 +78,29 @@ typedef int (*FDFSSendFile)(void *arg, const char *filename, \
 
 typedef int (*FDFSProxyHandler)(ngx_http_request_t *r, const char *dest_ip_addr);
 
+struct fdfs_http_resp_content_range {
+	int length;
+	char content[64];
+};
+
 struct fdfs_http_response {
 	int status;  //HTTP status
 	time_t last_modified;  //last modified time of the file
 	int redirect_url_len;
-	int range_len;
-	int content_range_len;
+	int range_len;  //for redirect
+	int boundary_len;
+        short content_range_count;
 	int64_t content_length;
 	char *content_type;
 	char *attachment_filename;
 	char redirect_url[256];
 	char content_disposition[128];
-	char range[64];
-	char content_range[64];
+	char content_type_buff[128];
+	char range[256];  //for redirect
+	char range_content_type[64];  //for multi range content
+    	struct fdfs_http_resp_content_range content_ranges[FDFS_MAX_HTTP_RANGES];
 	char last_modified_buff[32];
+    	char boundary[FDFS_MAX_HTTP_BOUNDARY];
 	bool header_outputed;   //if header output
 };
 
@@ -95,22 +111,24 @@ struct fdfs_http_range {
 
 struct fdfs_http_context {
 	int server_port;
+    	short range_count;
 	bool header_only;
 	bool if_range;
-	struct fdfs_http_range range;
+	struct fdfs_http_range ranges[FDFS_MAX_HTTP_RANGES];
 	char if_modified_since[32];
 	char *url;
 	void *arg; //for callback
 	FDFSOutputHeaders output_headers;
 	FDFSSendFile send_file;   //nginx send file
 	FDFSSendReplyChunk send_reply_chunk;
-	FDFSProxyHandler proxy_handler; //nginx proxy handler 
+	FDFSProxyHandler proxy_handler; //nginx proxy handler
 };
 
 struct fdfs_download_callback_args {
 	struct fdfs_http_context *pContext;
 	struct fdfs_http_response *pResponse;
 	int64_t sent_bytes;  //sent bytes
+        int range_index;
 };
 
 /**
@@ -142,10 +160,10 @@ int fdfs_http_request_handler(struct fdfs_http_context *pContext);
 * parse range parameter
 * params:
 *	value the range value
-*	rang the range object, store start and end position
+*	pContext the context
 * return: 0 success, !=0 fail, return the error code
 */
-int fdfs_parse_range(const char *value, struct fdfs_http_range *range);
+int fdfs_parse_ranges(const char *value, struct fdfs_http_context *pContext);
 
 #ifdef __cplusplus
 }
